38fb8a96780dd450fda480cac96dd1381511f749
cocomango-GH <111953271+Cocomango-GH@users.noreply.github.com>
2024-06-05 04:11:59 -0700

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import MovieDetailPage from './Pages/MovieDetailPage';
+import TvShowDetailPage from './Pages/TvShowDetailPage';
+import SearchResultsPage from './Pages/SearchResultsPage';
+import ProfilePage from './Pages/ProfilePage';
+import LogIn from './Pages/LogIn';
+import RegisterPage from './Pages/RegisterPage';
+import ForgotPasswordPage from './Pages/ForgotPasswordPage';
+import NotFoundPage from './Pages/NotFoundPage';
+import GenrePage from './Pages/GenrePage';
+import Watchlistpage from './Pages/WatchListpage';
+import SettingsPage from './Pages/SettingsPage';
+
 
 export default function App() {
   return (
-    <h1 className= 'text-3xl text-red-500'>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movie/:id" element={<MovieDetailPage />} />
+        <Route path="/tv/:id" element={<TvShowDetailPage />} />
+        <Route path="/search" element={<SearchResultsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/genre/:genre" element={<GenrePage />} />
+        <Route path="/watchlist" element={<Watchlistpage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/Pages/ForgotPassWordPage.jsx
+++ client/src/Pages/ForgotPassWordPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function ForgotPassWordPage() {
+export default function ForgotPasswordPage() {
   return (
-    <div>ForgotPassWordPage</div>
+    <div>ForgotPasswordPage</div>
   )
 }
